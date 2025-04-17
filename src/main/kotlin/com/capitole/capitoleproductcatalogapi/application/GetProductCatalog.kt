2f23c4430240c6c9bbf24155cb72b2f23a501631
jordiadan <jordiadan@gmail.com>
2025-04-17 12:00:03 +0200

    🔴 Implement basic mapping between domain and application layer

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/application/GetProductCatalog.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/application/GetProductCatalog.kt
@@ -1,11 +1,28 @@
 package com.capitole.capitoleproductcatalogapi.application
 
+import com.capitole.capitoleproductcatalogapi.domain.Product
 import com.capitole.capitoleproductcatalogapi.domain.ProductRepository
 
 class GetProductCatalog(private val productRepository: ProductRepository) {
   fun execute(): ProductCatalogDTO {
-    // TODO Implement use case instead of using dummy content
-    return ProductCatalogDTO(listOf())
+    val products = productRepository.findAll()
+    return products.toPRoductCatalogDTO()
   }
 
+}
+
+private fun List<Product>.toPRoductCatalogDTO(): ProductCatalogDTO {
+  val productDTOs = this.map { product ->
+    val discount = ""
+    val finalPrice = ""
+    ProductCatalogDTO.ProductDTO(
+        sku = product.sku.value,
+        description = product.description.value,
+        price = product.price.value.toString(),
+        discountPercentage = discount,
+        finalPrice = finalPrice,
+        category = product.category.name
+    )
+  }
+  return ProductCatalogDTO(productDTOs)
 }
