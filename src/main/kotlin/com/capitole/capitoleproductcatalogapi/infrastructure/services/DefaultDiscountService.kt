bf3b8372437c0e9a4fe4e4878db0698f0cc3d2c0
jordiadan <jordiadan@gmail.com>
2025-04-17 17:49:16 +0200

    🔵 Add temporary DefaultDiscountService

---
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/services/DefaultDiscountService.kt
@@ -0,0 +1,18 @@
+package com.capitole.capitoleproductcatalogapi.infrastructure.services
+
+import com.capitole.capitoleproductcatalogapi.domain.Category
+import com.capitole.capitoleproductcatalogapi.domain.DiscountPercentage
+import com.capitole.capitoleproductcatalogapi.domain.DiscountService
+import com.capitole.capitoleproductcatalogapi.domain.Product
+
+// TODO: Think about moving this class to domain, as there are a lot of business rules
+class DefaultDiscountService : DiscountService {
+  override fun getApplicableDiscount(product: Product): DiscountPercentage {
+    if (product.sku.value.endsWith("5")) return DiscountPercentage(30.0)
+    return when (product.category) {
+      Category.HOME_AND_KITCHEN -> DiscountPercentage(25.0)
+      Category.ELECTRONICS      -> DiscountPercentage(15.0)
+      else                      -> DiscountPercentage(0.0)
+    }
+  }
+}
