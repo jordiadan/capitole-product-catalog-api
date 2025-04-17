b4e0e9b1514d82a512b2be5024eeab84a755fa97
jordiadan <jordiadan@gmail.com>
2025-04-17 17:48:20 +0200

    🔵 Add missing decimals on expectedJson GetProductCatalogControllerTest

--- src/test/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/GetProductCatalogControllerTest.kt
+++ src/test/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/GetProductCatalogControllerTest.kt
@@ -38,7 +38,7 @@ class GetProductCatalogControllerTest {
                   "sku": "SKU0001",
                   "description": "Wireless Mouse with ergonomic design",
                   "price": "19.99",
-                  "discountPercentage": "0",
+                  "discountPercentage": "0.00",
                   "finalPrice": "19.99",
                   "category": "Electronics"
                 },
@@ -46,7 +46,7 @@ class GetProductCatalogControllerTest {
                   "sku": "SKU0005",
                   "description": "Noise-Cancelling Over-Ear Headphones",
                   "price": "120.00",
-                  "discountPercentage": "30",
+                  "discountPercentage": "30.00",
                   "finalPrice": "84.00",
                   "category": "Electronics"
                 }
