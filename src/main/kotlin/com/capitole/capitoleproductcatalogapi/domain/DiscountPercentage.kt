3ce3aeb591994929e012b7035ae57ce3868eb5a9
jordiadan <jordiadan@gmail.com>
2025-04-17 12:43:49 +0200

    🔵 Add TODO comment to reject invalid values

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/domain/DiscountPercentage.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/domain/DiscountPercentage.kt
@@ -1,3 +1,9 @@
 package com.capitole.capitoleproductcatalogapi.domain
 
-class DiscountPercentage(val value: Double)
+class DiscountPercentage(val value: Double) {
+/*
+   TODO should reject invalid values
+    1. Negative values
+    2. Above 100
+*/
+}
