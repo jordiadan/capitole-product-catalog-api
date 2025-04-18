c3527773fc67030fe98890c89a1b8dcdd27b9554
jordiadan <jordiadan@gmail.com>
2025-04-18 11:15:45 +0200

    🟢 Should return product catalog with proper discounts applied for all products

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/repositories/PostgresSQLProductRepository.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/repositories/PostgresSQLProductRepository.kt
@@ -1,11 +1,23 @@
 package com.capitole.capitoleproductcatalogapi.infrastructure.repositories
 
+import com.capitole.capitoleproductcatalogapi.domain.Category
+import com.capitole.capitoleproductcatalogapi.domain.Description
+import com.capitole.capitoleproductcatalogapi.domain.Price
 import com.capitole.capitoleproductcatalogapi.domain.Product
 import com.capitole.capitoleproductcatalogapi.domain.ProductRepository
+import com.capitole.capitoleproductcatalogapi.domain.SKU
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
 class PostgresSQLProductRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) : ProductRepository {
   override fun findAll(): List<Product> {
-    return listOf()
+    val sql = """SELECT sku, description, price, category FROM products""".trimIndent()
+    return jdbcTemplate.query(sql, { rs, _ ->
+      Product(
+          sku = SKU(rs.getString("sku")),
+          description = Description(rs.getString("description")),
+          price = Price(rs.getDouble("price")),
+          category = Category.valueOf(rs.getString("category")),
+      )
+    })
   }
 }
