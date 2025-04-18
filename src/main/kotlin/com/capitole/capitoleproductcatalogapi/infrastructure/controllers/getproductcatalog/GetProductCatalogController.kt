22b7f229a68f4c47865f82ec2a535f678643cbed
jordiadan <jordiadan@gmail.com>
2025-04-18 12:44:02 +0200

    🟢 Should return 400 when category is invalid

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/controllers/getproductcatalog/GetProductCatalogController.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/controllers/getproductcatalog/GetProductCatalogController.kt
@@ -1,17 +1,21 @@
 package com.capitole.capitoleproductcatalogapi.infrastructure.controllers.getproductcatalog
 
 import com.capitole.capitoleproductcatalogapi.application.GetProductCatalog
+import com.capitole.capitoleproductcatalogapi.domain.Category
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GetProductCatalogController(private val getProductCatalog: GetProductCatalog) {
 
   @GetMapping("/products")
-  fun getProductCatalog(): ResponseEntity<GetProductCatalogResponse> {
-    val getProductCatalogDTO = getProductCatalog.execute()
+  fun getProductCatalog(
+    @RequestParam(name = "category", required = false) category: Category?
+  ): ResponseEntity<GetProductCatalogResponse> {
+    val getProductCatalogDTO = getProductCatalog.execute(category)
     val getProductCatalogResponse = getProductCatalogDTO.toGetProductCatalogResponse()
     return ResponseEntity.ok()
         .contentType(MediaType.APPLICATION_JSON)
