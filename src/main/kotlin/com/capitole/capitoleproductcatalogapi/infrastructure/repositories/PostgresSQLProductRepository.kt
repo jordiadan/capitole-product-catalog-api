a221cd052d65647d11857b40c3f15c3b37ca55ed
jordiadan <jordiadan@gmail.com>
2025-04-18 13:18:11 +0200

    🔵 Improve PostgresSQLProductRepository readability

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/repositories/PostgresSQLProductRepository.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/repositories/PostgresSQLProductRepository.kt
@@ -6,33 +6,37 @@ import com.capitole.capitoleproductcatalogapi.domain.Price
 import com.capitole.capitoleproductcatalogapi.domain.Product
 import com.capitole.capitoleproductcatalogapi.domain.ProductRepository
 import com.capitole.capitoleproductcatalogapi.domain.SKU
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
 class PostgresSQLProductRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) : ProductRepository {
   override fun findAll(categoryFilter: Category?): List<Product> {
-    val sql = StringBuilder().apply {
-      append("""
-        SELECT sku,
-               description,
-               price,
-               category
-          FROM products
-      """.trimIndent())
-      if (categoryFilter != null) {
-        append("\n WHERE category = CAST(:category AS category_enum)")
-      }
-    }.toString()
-
-    val params = mutableMapOf<String, Any>()
-    categoryFilter?.let { params["category"] = it.name }
-
-    return jdbcTemplate.query(sql, params) { rs, _ ->
-      Product(
-          sku         = SKU(rs.getString("sku")),
-          description = Description(rs.getString("description")),
-          price       = Price(rs.getDouble("price")),
-          category    = Category.valueOf(rs.getString("category"))
+    val sql = buildString {
+      append(
+          """
+          SELECT sku,
+                 description,
+                 price,
+                 category
+            FROM products
+        """.trimIndent()
       )
+      categoryFilter?.also {
+        append("\nWHERE category = CAST(:category AS category_enum)")
+      }
     }
+
+    val params = categoryFilter?.let { mapOf("category" to it.name) } ?: emptyMap()
+
+    return jdbcTemplate.query(sql, params, productRowMapper)
+  }
+
+  private val productRowMapper = RowMapper<Product> { rs, _ ->
+    Product(
+        sku = SKU(rs.getString("sku")),
+        description = Description(rs.getString("description")),
+        price = Price(rs.getDouble("price")),
+        category = Category.valueOf(rs.getString("category"))
+    )
   }
 }
