ea37d555ec1476019bd986cadf54f6eb69b3fe81
jordiadan <jordiadan@gmail.com>
2025-04-20 22:39:41 +0200

    🔵 Add Profile configuration to test OpenApi on "production" with dummy data

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/configuration/InfrastructureConfiguration.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/configuration/InfrastructureConfiguration.kt
@@ -1,20 +1,31 @@
 package com.capitole.capitoleproductcatalogapi.infrastructure.configuration
 
 import com.capitole.capitoleproductcatalogapi.domain.product.ProductRepository
+import com.capitole.capitoleproductcatalogapi.infrastructure.repositories.product.InMemoryProductRepository
 import com.capitole.capitoleproductcatalogapi.infrastructure.repositories.product.PostgresSQLProductRepository
 import com.capitole.capitoleproductcatalogapi.infrastructure.repositories.product.ProductSQLBuilder
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
 @Configuration
 class InfrastructureConfiguration {
-    @Bean
-    fun productRepository(
-        jdbcTemplate: NamedParameterJdbcTemplate,
-        productSQLBuilder: ProductSQLBuilder
-    ): ProductRepository =
-        PostgresSQLProductRepository(jdbcTemplate, productSQLBuilder)
+
+  // TODO: Just to test "main" environment with dummy data (OpenApi)
+  @Bean
+  @Profile("!integration‑test")
+  fun inMemoryProductRepository() = InMemoryProductRepository()
+
+  @Bean
+  @Profile("integration‑test")
+  fun productRepository(
+    jdbcTemplate: NamedParameterJdbcTemplate,
+    productSQLBuilder: ProductSQLBuilder
+  ): ProductRepository =
+      PostgresSQLProductRepository(jdbcTemplate, productSQLBuilder)
 
   @Bean
   fun productSQLBuilder() = ProductSQLBuilder()
