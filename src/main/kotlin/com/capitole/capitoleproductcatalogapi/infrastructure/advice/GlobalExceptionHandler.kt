f6ccb0bdf45bb4efcc664c0455cb4bfb44c4cc00
jordiadan <jordiadan@gmail.com>
2025-04-20 17:08:49 +0200

    🟢 Should return 400 when category is invalid with error message

---
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/advice/GlobalExceptionHandler.kt
@@ -0,0 +1,16 @@
+package com.capitole.capitoleproductcatalogapi.infrastructure.advice
+
+import com.capitole.capitoleproductcatalogapi.domain.product.CategoryNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+  @ExceptionHandler(CategoryNotFoundException::class)
+  fun handleInvalidCategory(ex: CategoryNotFoundException): ResponseEntity<ErrorResponse> =
+      ErrorResponse(400, ex.message ?: "Category not found")
+          .let { ResponseEntity.status(HttpStatus.BAD_REQUEST).body(it) }
+}
