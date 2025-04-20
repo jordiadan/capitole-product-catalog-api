1468a1b4052b57ec84515116d3853cbc8507c4b5
jordiadan <jordiadan@gmail.com>
2025-04-20 21:54:11 +0200

    🔵 Make rules private

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/domain/discount/DefaultDiscountService.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/domain/discount/DefaultDiscountService.kt
@@ -3,7 +3,7 @@ package com.capitole.capitoleproductcatalogapi.domain.discount
 import com.capitole.capitoleproductcatalogapi.domain.discount.rules.DiscountRule
 import com.capitole.capitoleproductcatalogapi.domain.product.Product
 
-class DefaultDiscountService(val rules: List<DiscountRule>) : DiscountService {
+class DefaultDiscountService(private val rules: List<DiscountRule>) : DiscountService {
   override fun getApplicableDiscount(product: Product): DiscountPercentage =
       rules.map { it.applyTo(product) }.maxByOrNull { it.value } ?: DiscountPercentage(0.0)
 }
