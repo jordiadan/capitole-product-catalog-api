f13b0b33103550009e24d0679357fceb15fd66ae
jordiadan <jordiadan@gmail.com>
2025-04-20 21:44:12 +0200

    🔵 Move category discounts to application.yml

--- src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/configuration/DomainConfiguration.kt
+++ src/main/kotlin/com/capitole/capitoleproductcatalogapi/infrastructure/configuration/DomainConfiguration.kt
@@ -6,16 +6,19 @@ import com.capitole.capitoleproductcatalogapi.domain.discount.rules.CategoryRule
 import com.capitole.capitoleproductcatalogapi.domain.discount.rules.DiscountRule
 import com.capitole.capitoleproductcatalogapi.domain.discount.rules.SkuEndsWith5Rule
 import com.capitole.capitoleproductcatalogapi.domain.product.Category
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class DomainConfiguration {
   @Bean
-  fun electronicsRule() = CategoryRule(Category.ELECTRONICS, 15.0)
+  fun electronicsRule(@Value("\${discount.category-rules.ELECTRONICS}") discount: Double) =
+      CategoryRule(Category.ELECTRONICS, discount)
 
   @Bean
-  fun homeAndKitchenRule() = CategoryRule(Category.HOME_AND_KITCHEN, 25.0)
+  fun homeAndKitchenRule(@Value("\${discount.category-rules.HOME_AND_KITCHEN}") discount: Double) =
+      CategoryRule(Category.HOME_AND_KITCHEN, discount)
 
   @Bean
   fun specialSkuRule() = SkuEndsWith5Rule()
